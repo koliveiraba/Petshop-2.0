37a697c1d039ea3424650a7defe4db8e36186f3a
koliveiraba <klismann.professional@gmail.com>
2024-06-09 15:15:49 -0300

    removed unused imports

--- Petshop/src/main/java/com/example/Petshop/controllers/PetController.java
+++ Petshop/src/main/java/com/example/Petshop/controllers/PetController.java
@@ -2,13 +2,9 @@ package com.example.Petshop.controllers;
 
 import com.example.Petshop.Service.PetService;
 import com.example.Petshop.domain.tutor.pet.Pet;
-import com.example.Petshop.domain.tutor.pet.PetRepository;
-import com.example.Petshop.domain.tutor.pet.Tutor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
